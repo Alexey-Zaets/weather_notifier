84070ff461b0b4e7fc45b3ebd65239ac62085f0b
Alexey-Zaets <3aets.dev@gmail.com>
2018-07-17 16:59:05 +0300

    version-1.0

---
+++ current_weather.py
@@ -0,0 +1,36 @@
+"""This is a weather notifier for UNIX-like OS.
+
+source.icons --  is a dictionary with paths for weather icons,
+source.city --  is a name of your city,
+source.key --  is an API key.
+
+"""
+
+from source import key, city, icons 
+import time, subprocess, requests
+
+def weather_message():
+    """This function creates and sends messages of the  current weather forecast to the desktop."""
+    api_url = 'http://api.openweathermap.org/data/2.5/weather'
+    params = {
+            'q': city, 
+            'units': 'metric', 
+            'appid': key
+            }
+    request = requests.get(api_url, params=params)
+    data = request.json()
+    try:
+        image = icons[data['weather'][0]['description']]
+        data = data['main']
+        title = 'Current weather in {}:'.format(city)
+        template = 'Temperature: {} C\nHumidity: {} %\nAtmospheric pressure: {} hPa\n'
+        message = template.format(round(data['temp']), data['humidity'], data['pressure'])
+    except KeyError:
+        subprocess.call(['notify-send', 'Error'])
+    subprocess.call(['notify-send', image, title, message])
+    time.sleep(3600.0)
+    weather_message()
+
+
+if __name__ == '__main__':
+    weather_message()
---
+++ source.py
@@ -0,0 +1,16 @@
+
+key = 'a1caef73cfbebec2e0e07942a0896137'
+
+city = 'Zaporizhzhya'
+
+icons = {
+        'clear sky': '--icon=/home/zaets/weather_notifier/icons/weather-clear.svg',
+        'few clouds': '--icon=/home/zaets/weather_notifier/icons/weather-few-clouds.svg',
+        'scattered clouds': '--icon=/home/zaets/weather_notifier/icons/weather-clouds.swg',
+        'broken clouds': '--icon=/home/zaets/weather_notifier/icons/weather-overcast.svg',
+        'shower rain': '--icon=/home/zaets/weather_notifier/icons/weather-shower-scattered.svg',
+        'rain': '--icon=/home/zaets/weather_notifier/icons/weather-shower.svg',
+        'thunderstorm': '--icon=/home/zaets/weather_notifier/icons/weather-storm.svg',
+        'snow': '--icon=/home/zaets/weather_notifier/icons/weather-snow.svg',
+        'mist': '--icon=/home/zaets/weather_notifier/icons/weather-fog.svg'
+        }
