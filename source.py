188027fb14d1adcd36de85b8543c8d0d7d096af8
Alexey-Zaets <3aets.dev@gmail.com>
2018-07-17 17:04:10 +0300

    changed source.py

--- source.py
+++ source.py
@@ -1,16 +1,16 @@
 
-key = 'a1caef73cfbebec2e0e07942a0896137'
+key = ''  # API key
 
-city = 'Zaporizhzhya'
+city = ''  # city name
 
 icons = {
-        'clear sky': '--icon=/home/zaets/weather_notifier/icons/weather-clear.svg',
-        'few clouds': '--icon=/home/zaets/weather_notifier/icons/weather-few-clouds.svg',
-        'scattered clouds': '--icon=/home/zaets/weather_notifier/icons/weather-clouds.swg',
-        'broken clouds': '--icon=/home/zaets/weather_notifier/icons/weather-overcast.svg',
-        'shower rain': '--icon=/home/zaets/weather_notifier/icons/weather-shower-scattered.svg',
-        'rain': '--icon=/home/zaets/weather_notifier/icons/weather-shower.svg',
-        'thunderstorm': '--icon=/home/zaets/weather_notifier/icons/weather-storm.svg',
-        'snow': '--icon=/home/zaets/weather_notifier/icons/weather-snow.svg',
-        'mist': '--icon=/home/zaets/weather_notifier/icons/weather-fog.svg'
+        'clear sky': '--icon=~/weather_notifier/icons/weather-clear.svg',
+        'few clouds': '--icon=~/weather_notifier/icons/weather-few-clouds.svg',
+        'scattered clouds': '--icon=~/weather_notifier/icons/weather-clouds.swg',
+        'broken clouds': '--icon=~/weather_notifier/icons/weather-overcast.svg',
+        'shower rain': '--icon=~/weather_notifier/icons/weather-shower-scattered.svg',
+        'rain': '--icon=~/weather_notifier/icons/weather-shower.svg',
+        'thunderstorm': '--icon=~/weather_notifier/icons/weather-storm.svg',
+        'snow': '--icon=~/weather_notifier/icons/weather-snow.svg',
+        'mist': '--icon=~/weather_notifier/icons/weather-fog.svg'
         }
